cc12e842539e071a25aaed9ff23d44bf4cf3906b
Matthew Faigan <matthew.faigan@gmail.com>
2018-03-05 14:52:46 -0500

    Finish chisel module

--- Chisel/Chisel/Entities/ChiselModule.cs
+++ Chisel/Chisel/Entities/ChiselModule.cs
@@ -22,19 +22,53 @@ using System.Threading.Tasks;
 
 namespace Chisel.Entities
 {
+    /// <summary>
+    /// Represents a base class for all Chisel modules.
+    /// </summary>
     public abstract class ChiselModule
     {
+        /// <summary>
+        /// The scraping targets that this module will handle.
+        /// </summary>
         public abstract IEnumerable<string> Targets { get; }
 
+        /// <summary>
+        /// The base URL (empty by default) that each target should be appended to.
+        /// </summary>
         public string BaseUrl { get; protected set; } 
             = string.Empty;
 
-        public Dictionary<string, string> Headers { get; } 
-            = new Dictionary<string, string>();
+        /// <summary>
+        /// The headers to use when sending requests.
+        /// </summary>
+        public Dictionary<string, IEnumerable<string>> Headers { get; } 
+            = new Dictionary<string, IEnumerable<string>>();
 
+        /// <summary>
+        /// The minimum time to wait between consecutive requests.
+        /// </summary>
         public int Backoff { get; protected set; } = 1000;
 
+        /// <summary>
+        /// Can optionally be overriden to execute code after module creation, before scraping.
+        /// </summary>
+        /// <param name="provider">An <see cref="IServiceProvider"/> instance that contains services added by the user.</param>
         public virtual Task Init(IServiceProvider provider)
+        {
+            // Left blank for optional client implementation.
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Will be called after each target is retrieved, in order to handle it.
+        /// </summary>
+        /// <param name="response">A <see cref="ChiselResponse"/> containing the response headers, status code, and body.</param>
+        public abstract Task Handle(ChiselResponse response);
+
+        /// <summary>
+        /// Can optionally be overriden to execute code after scraping has finished.
+        /// </summary>
+        public virtual Task AfterScraping()
         {
             // Left blank for optional client implementation.
             return Task.CompletedTask;
